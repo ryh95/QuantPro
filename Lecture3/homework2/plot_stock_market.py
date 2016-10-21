e9490a22401cd6a5a0fd3c3e30185410adad8a1c
ryh <1971990184@qq.com>
2016-10-21 21:20:17 +0800

    add homework2 result

--- Lecture3/homework2/plot_stock_market.py
+++ Lecture3/homework2/plot_stock_market.py
@@ -1,3 +1,4 @@
+# coding:utf8
 """
 =======================================
 Visualizing the stock market structure
@@ -59,7 +60,9 @@ is to position the labels minimizing overlap. For this we use an
 heuristic based on the direction of the nearest neighbor along each
 axis.
 """
-print(__doc__)
+from pymongo import MongoClient
+
+# print(__doc__)
 
 # Author: Gael Varoquaux gael.varoquaux@normalesup.org
 # License: BSD 3 clause
@@ -77,83 +80,38 @@ from matplotlib.collections import LineCollection
 from sklearn import cluster, covariance, manifold
 
 ###############################################################################
-# Retrieve the data from Internet
+# Retrieve the data from MongoDB
 
-# Choose a time period reasonably calm (not too long ago so that we get
-# high-tech firms, and before the 2008 crash)
-d1 = datetime.datetime(2003, 1, 1)
-d2 = datetime.datetime(2008, 1, 1)
+client = MongoClient()
+db = client['stockcodes']
+coll = db.HS300.find()
 
-# kraft symbol has now changed from KFT to MDLZ in yahoo
-symbol_dict = {
-    'TOT': 'Total',
-    'XOM': 'Exxon',
-    'CVX': 'Chevron',
-    'COP': 'ConocoPhillips',
-    # 'VLO': 'Valero Energy',
-    # 'MSFT': 'Microsoft',
-    # 'IBM': 'IBM',
-    # 'TWX': 'Time Warner',
-    # 'CMCSA': 'Comcast',
-    # 'CVC': 'Cablevision',
-    # 'YHOO': 'Yahoo',
-    # 'DELL': 'Dell',
-    # 'HPQ': 'HP',
-    # 'AMZN': 'Amazon',
-    # 'TM': 'Toyota',
-    # 'CAJ': 'Canon',
-    # 'MTU': 'Mitsubishi',
-    # 'SNE': 'Sony',
-    # 'F': 'Ford',
-    # 'HMC': 'Honda',
-    # 'NAV': 'Navistar',
-    # 'NOC': 'Northrop Grumman',
-    # 'BA': 'Boeing',
-    # 'KO': 'Coca Cola',
-    # 'MMM': '3M',
-    # 'MCD': 'Mc Donalds',
-    # 'PEP': 'Pepsi',
-    # 'MDLZ': 'Kraft Foods',
-    # 'K': 'Kellogg',
-    # 'UN': 'Unilever',
-    # 'MAR': 'Marriott',
-    # 'PG': 'Procter Gamble',
-    # 'CL': 'Colgate-Palmolive',
-    # 'GE': 'General Electrics',
-    # 'WFC': 'Wells Fargo',
-    # 'JPM': 'JPMorgan Chase',
-    # 'AIG': 'AIG',
-    # 'AXP': 'American express',
-    # 'BAC': 'Bank of America',
-    # 'GS': 'Goldman Sachs',
-    # 'AAPL': 'Apple',
-    # 'SAP': 'SAP',
-    # 'CSCO': 'Cisco',
-    # 'TXN': 'Texas instruments',
-    # 'XRX': 'Xerox',
-    # 'LMT': 'Lookheed Martin',
-    # 'WMT': 'Wal-Mart',
-    # 'WBA': 'Walgreen',
-    # 'HD': 'Home Depot',
-    # 'GSK': 'GlaxoSmithKline',
-    # 'PFE': 'Pfizer',
-    # 'SNY': 'Sanofi-Aventis',
-    # 'NVS': 'Novartis',
-    'KMB': 'Kimberly-Clark',
-    'R': 'Ryder',
-    'GD': 'General Dynamics',
-    'RTN': 'Raytheon',
-    'CVS': 'CVS',
-    'CAT': 'Caterpillar',
-    'DD': 'DuPont de Nemours'}
+
+symbol_dict = {}
+
+for document in coll:
+    symbol_dict[document['stockcode']] = document['name']
+
 
 symbols, names = np.array(list(symbol_dict.items())).T
 
-quotes = [quotes_historical_yahoo_ochl(symbol, d1, d2, asobject=True)
-          for symbol in symbols]
 
-open = np.array([q.open for q in quotes]).astype(np.float)
-close = np.array([q.close for q in quotes]).astype(np.float)
+db = client['data']
+
+close = []
+open = []
+for symbol in symbols:
+    coll =  db[symbol].find()
+    close_symbol = []
+    open_symbol = []
+    for document in coll:
+        close_symbol.append(document['close'])
+        open_symbol.append(document['open'])
+    close.append(close_symbol)
+    open.append(open_symbol)
+
+close = np.array(close).astype(np.float)
+open = np.array(open).astype(np.float)
 
 # Check if has nan in open or close
 # print np.isnan(np.min(close))
@@ -226,8 +184,8 @@ ax.add_collection(lc)
 
 # Add a label to each node. The challenge here is that we want to
 # position the labels to avoid overlap with other labels
-for index, (name, label, (x, y)) in enumerate(
-        zip(names, labels, embedding.T)):
+for index, (symbol, label, (x, y)) in enumerate(
+        zip(symbols, labels, embedding.T)):
 
     dx = x - embedding[0]
     dx[index] = 1
@@ -247,12 +205,12 @@ for index, (name, label, (x, y)) in enumerate(
     else:
         verticalalignment = 'top'
         y = y - .002
-    plt.text(x, y, name, size=10,
+    plt.text(x, y, symbol, size=10,
              horizontalalignment=horizontalalignment,
              verticalalignment=verticalalignment,
              bbox=dict(facecolor='w',
                        edgecolor=plt.cm.spectral(label / float(n_labels)),
-                       alpha=.6))
+                           alpha=.6))
 
 plt.xlim(embedding[0].min() - .15 * embedding[0].ptp(),
          embedding[0].max() + .10 * embedding[0].ptp(),)
---
+++ Lecture3/homework2/test/plt_text.py
@@ -0,0 +1,11 @@
+# coding:utf8
+import matplotlib.pyplot as plt
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+plt.text(0.1,1,u'你好')
+plt.show()
+
+# print type('hello'),type('你好')
