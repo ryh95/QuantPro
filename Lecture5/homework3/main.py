dbadad87ae601e5793be3361adec36455d5684e7
ryh <1971990184@qq.com>
2016-11-03 11:04:37 +0800

    add adaboost

--- Lecture5/homework3/main.py
+++ Lecture5/homework3/main.py
@@ -2,11 +2,11 @@
 
 import pandas as pd
 import numpy as np
-import xgboost
+# import xgboost
 from matplotlib import pyplot
 from sklearn.metrics import r2_score
 
-from xgboost import plot_importance
+# from xgboost import plot_importance
 
 df = pd.read_csv('./data/HFT_XY_unselected.csv') #从readme.txt中下载
 col_names_x = df.columns[1:333]
@@ -19,6 +19,10 @@ print x_all.shape
 print y_all.shape
 
 # train test split
+# apply minmax scale
+# from sklearn.preprocessing import minmax_scale
+# minmax_scale(x_all,copy=False)
+
 train_x = x_all[0:x_all.shape[0]*7/10]
 train_y = y_all[0:y_all.shape[0]*7/10]
 test_x = x_all[x_all.shape[0]*7/10:]
@@ -32,23 +36,21 @@ print test_y.shape
 # try some models e.g. XGBoost
 
 # fit model no training data
-model = xgboost.XGBRegressor(n_estimators=3000,learning_rate=0.07)
-
-eval_set = [(test_x,test_y)]
-model.fit(train_x, train_y,eval_metric="logloss", eval_set=eval_set, verbose=True,early_stopping_rounds=10)
-# make predictions for test data
-y_pred = model.predict(test_x)
-# evaluate predictions
-accuracy = r2_score(test_y, y_pred)
-print("Accuracy: %.2f" % (accuracy * 100.0))
-
-###########################################
-# Todo:plot learning curve
-
-###########################################
-# plot feature importance
-plot_importance(model)
-pyplot.show()
+# model = xgboost.XGBRegressor(n_estimators=3000,learning_rate=0.07)
+#
+# eval_set = [(test_x,test_y)]
+# model.fit(train_x, train_y,eval_metric="logloss", eval_set=eval_set, verbose=True,early_stopping_rounds=10)
+# # make predictions for test data
+# y_pred = model.predict(test_x)
+# # evaluate predictions
+# accuracy = r2_score(test_y, y_pred)
+# print("Accuracy: %.2f" % (accuracy * 100.0))
+#
+# # Todo:plot learning curve
+#
+# # plot feature importance
+# plot_importance(model)
+# pyplot.show()
 
 ###########################################
 #Now let's Learn some benchmark models...
@@ -60,3 +62,24 @@ from sklearn.metrics import r2_score,mean_squared_error
 ols = linear_model.LinearRegression()
 pred_ols = ols.fit(train_x,train_y).predict(test_x)
 print("Accuracy: %.2f (for OLS)" % (r2_score(test_y,pred_ols) * 100.0))
+
+###########################################
+# try SVR
+# from sklearn import svm
+# svr = svm.SVR(kernel='linear',cache_size=2000)
+# pred_svr = svr.fit(train_x,train_y).predict(test_x)
+# print("Accuracy: %.2f (for SVR)" % (r2_score(test_y,pred_svr) * 100.0))
+
+###########################################
+# Todo:try ANN(MLP)
+import keras
+
+
+###########################################
+# try adaboost
+from sklearn import ensemble
+from sklearn import tree
+ada_regressor = ensemble.AdaBoostRegressor(base_estimator=tree.DecisionTreeRegressor(max_depth=17),n_estimators=3000,random_state=4)
+pred_ada = ada_regressor.fit(train_x,train_y).predict(test_x)
+print("Accuracy: %.2f (for AdaBoost)" % (r2_score(test_y,pred_ada) * 100.0))
+
